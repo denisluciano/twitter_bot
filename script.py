0fbe424a9ec9a66d54132bdc43d79b1231d90c74
Denis Luciano <denis.lopes3012@gmail.com>
2020-03-23 09:09:44 -0300

    add exeption in

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 # import libraries
 import tweepy
 
-# set credentials
+texto = "Essa mensagem foi postada porde sum bots s"
+
 consumer_secret = "5kCk5tC6OMZfxTHILeZ517FzhzEB1bHT4hxWfEphl6clMVCUEd"
 consumer_key = "J0E5BDn1Tb14m41dBBgBqHa9S"           #Credencias do twitter dev
 access_token = "1179805875701272581-E3bHjsiNuCcT3yUk6SIakyDjYastMq"
@@ -12,8 +13,8 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)          #Logando 
 
 
-texto = "Bom dia para todos menos para @lBlack_11"
-
-# Postando de fato no twitter
-api.update_status(status=texto) 
+try:
+  res = api.update_status(status=texto)
+except tweepy.TweepError as e:
+  print (e)
 
